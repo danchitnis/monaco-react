fd7d468a0992a2672db257bd8d6d1b0c90d5b72e
danchitnis <danchitnis@users.noreply.github.com>
2020-12-12 07:13:34 +0000

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Editor from '@monaco-editor/react';
+import { monaco } from '@monaco-editor/react';
+import EditorNew from './editorNew';
+import type * as monacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
 
 interface AppProps {}
 
 function App({}: AppProps) {
   // Create the count state.
+  const editorRef = React.useRef<monacoEditor.editor.IStandaloneCodeEditor>();
 
   // Create the counter (+1 every second).
+  /*function handleEditorDidMount(
+    getValue: () => string,
+    editor: monacoEditor.editor.IStandaloneCodeEditor,
+  ): string {
+    return '';
+  }*/
 
+  const handleEditorDidMount = (): string => {
+    return '';
+  };
   // Return the App component.
   return (
-    <div className="App">
-      <Editor theme="vs-dark" height="90vh" language="javascript" />
-    </div>
+    <>
+      <EditorNew
+        //value="loloello"
+        theme="vs-dark"
+        height="50vh"
+        language="javascript"
+        editorDidMount={handleEditorDidMount}
+      />
+    </>
   );
 }
 
---
+++ src/editorNew.tsx
@@ -0,0 +1,202 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { monaco } from '@monaco-editor/react';
+import type * as MonacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
+
+type EditorType = {
+  value?: string;
+  language?: string;
+  editorDidMount: () => string;
+  theme?: string;
+  line?: number;
+  width?: string;
+  height?: string;
+  options?: object;
+  className?: string;
+  wrapperClassName?: string;
+  overrideServices?: object;
+};
+
+const EditorNew = ({
+  value,
+  language,
+  editorDidMount,
+  theme,
+  line,
+  width,
+  height,
+  options,
+  overrideServices,
+  className,
+  wrapperClassName,
+}: EditorType) => {
+  const [isMonacoReady, setIsMonacoReady] = useState(false);
+  const [isEditorCodeMounted, setIsEditorCodeMounted] = useState(false);
+  const editorCodeRef = useRef<MonacoEditor.editor.IStandaloneCodeEditor>();
+  const editorRef = useRef<typeof MonacoEditor.editor>();
+  const monacoRef = useRef<typeof MonacoEditor>();
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const f = async () => {
+      const monacoEditor = await monaco.init();
+      monacoRef.current = monacoEditor;
+      editorRef.current = monacoEditor.editor;
+      setIsMonacoReady(true);
+    };
+    f();
+  }, []);
+
+  useEffect(() => {
+    if (monacoRef.current && containerRef.current) {
+      editorCodeRef.current = monacoRef.current.editor.create(
+        containerRef.current,
+        {
+          value:
+            "// First line\nfunction hello() {\n\talert('Hello world!');\n}\n// Last line",
+          language: 'javascript',
+          roundedSelection: false,
+          scrollBeyondLastLine: false,
+          readOnly: false,
+          theme: 'vs-dark',
+          automaticLayout: true,
+        },
+      );
+      setIsEditorCodeMounted(true);
+    }
+  }, [isMonacoReady, containerRef]);
+
+  useEffect(() => {
+    if (editorRef.current && editorCodeRef.current && isEditorCodeMounted) {
+      console.log('🤩');
+
+      console.log(editorCodeRef.current.getModel());
+      const modelDefault = editorRef.current.createModel(
+        'let a=0\n',
+        language ? language : 'plaintext',
+      );
+      const model = editorCodeRef.current.getModel();
+      editorRef.current.setModelLanguage(
+        model ? model : modelDefault,
+        language ? language : 'plaintext',
+      );
+      editorCodeRef.current.setValue(value ? value : 'hello!');
+    }
+  }, [isEditorCodeMounted, theme, language, value]);
+
+  /*useEffect(() => {
+    if (editorRef.current && isMonacoMounted) {
+      editorRef.current.setTheme(theme ? theme : 'vs-dark');
+      const model = editorRef.current.createModel(value ? value : 'hello!');
+      editorRef.current.setModelLanguage(
+        model,
+        language ? language : 'plaintext',
+      );
+      console.log(value);
+      console.log(editorRef.current.getValue());
+    }
+  }, [isMonacoMounted, theme, language, value]);*/
+
+  /*useEffect(() => {
+    editorRef.current.updateOptions(options);
+  }, [isEditorReady, options]);*/
+
+  /*useUpdate(
+    () => {
+      if (
+        editorRef.current.getOption(
+          monacoRef.current.editor.EditorOption.readOnly,
+        )
+      ) {
+        editorRef.current.setValue(value);
+      } else {
+        if (value !== editorRef.current.getValue()) {
+          editorRef.current.executeEdits('', [
+            {
+              range: editorRef.current.getModel().getFullModelRange(),
+              text: value,
+            },
+          ]);
+
+          if (_isControlledMode) {
+            const model = editorRef.current.getModel();
+
+            model.forceTokenization(model.getLineCount());
+          }
+
+          editorRef.current.pushUndoStop();
+        }
+      }
+    },
+    [value],
+    isEditorReady,
+  );*/
+
+  /*useUpdate(
+    () => {
+      monacoRef.current.editor.setModelLanguage(
+        editorRef.current.getModel(),
+        language,
+      );
+    },
+    [language],
+    isEditorReady,
+  );
+
+  useUpdate(
+    () => {
+      editorRef.current.setScrollPosition({ scrollTop: line });
+    },
+    [line],
+    isEditorReady,
+  );
+
+  useUpdate(
+    () => {
+      monacoRef.current.editor.setTheme(theme);
+    },
+    [theme],
+    isEditorReady,
+  );*/
+
+  /*const createEditor = useCallback(() => {
+    editorRef.current = monacoRef.current.editor.create(
+      containerRef.current,
+      {
+        value,
+        language,
+        automaticLayout: true,
+        ...options,
+      },
+      overrideServices,
+    );
+
+    editorDidMount(
+      editorRef.current.getValue.bind(editorRef.current),
+      editorRef.current,
+    );
+
+    monacoRef.current.editor.defineTheme('dark', themes['night-dark']);
+    monacoRef.current.editor.setTheme(theme);
+
+    setIsEditorReady(true);
+  }, [editorDidMount, language, options, overrideServices, theme, value]);*/
+
+  /*useEffect(() => {
+    !isMonacoMounting && !isEditorReady && createEditor();
+  }, [isMonacoMounting, isEditorReady, createEditor]);*/
+
+  //const disposeEditor = () => editorRef.current.dispose();
+
+  return (
+    <div
+      style={{
+        display: 'block',
+        height,
+        width,
+      }}
+      ref={containerRef}
+    ></div>
+  );
+};
+
+export default EditorNew;
