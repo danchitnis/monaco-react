ecd2a7daf599a49ab43917b48941db52442a3e2b
danchitnis <danchitnis@users.noreply.github.com>
2020-12-16 14:51:05 +0000

    sample

--- src/editorNew.tsx
+++ src/editorNew.tsx
@@ -2,6 +2,12 @@ import React, { useState, useEffect, useRef } from 'react';
 import { monaco } from '@monaco-editor/react';
 import type * as MonacoEditor from 'monaco-editor/esm/vs/editor/editor.api';
 
+// https://www.gitmemory.com/issue/microsoft/monaco-editor/1423/530617327
+interface MonarchLanguageConfiguration
+  extends MonacoEditor.languages.IMonarchLanguage {
+  keywords: string[];
+}
+
 type EditorType = {
   value?: string;
   language?: string;
@@ -40,6 +46,175 @@ const EditorNew = ({
       const monacoEditor = await monaco.init();
       monacoRef.current = monacoEditor;
       editorRef.current = monacoEditor.editor;
+
+      monacoEditor.languages.register({ id: 'spice' });
+      monacoEditor.languages.setMonarchTokensProvider('spice', {
+        defaultToken: 'invalid',
+        keywords: [
+          'abstract',
+          'continue',
+          'for',
+          'new',
+          'switch',
+          'assert',
+          'goto',
+          'do',
+          'if',
+          'private',
+          'this',
+          'break',
+          'protected',
+          'throw',
+          'else',
+          'public',
+          'enum',
+          'return',
+          'catch',
+          'try',
+          'interface',
+          'static',
+          'class',
+          'finally',
+          'const',
+          'super',
+          'while',
+          'true',
+          'false',
+        ],
+
+        typeKeywords: [
+          'boolean',
+          'double',
+          'byte',
+          'int',
+          'short',
+          'char',
+          'void',
+          'long',
+          'float',
+        ],
+
+        operators: [
+          '=',
+          '>',
+          '<',
+          '!',
+          '~',
+          '?',
+          ':',
+          '==',
+          '<=',
+          '>=',
+          '!=',
+          '&&',
+          '||',
+          '++',
+          '--',
+          '+',
+          '-',
+          '*',
+          '/',
+          '&',
+          '|',
+          '^',
+          '%',
+          '<<',
+          '>>',
+          '>>>',
+          '+=',
+          '-=',
+          '*=',
+          '/=',
+          '&=',
+          '|=',
+          '^=',
+          '%=',
+          '<<=',
+          '>>=',
+          '>>>=',
+        ],
+
+        // we include these common regular expressions
+        symbols: /[=><!~?:&|+\-*\/\^%]+/,
+
+        // C# style strings
+        escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+
+        // The main tokenizer for our languages
+        tokenizer: {
+          root: [
+            // identifiers and keywords
+            [
+              /[a-z_$][\w$]*/,
+              {
+                cases: {
+                  '@typeKeywords': 'keyword',
+                  '@keywords': 'keyword',
+                  '@default': 'identifier',
+                },
+              },
+            ],
+            [/[A-Z][\w\$]*/, 'type.identifier'], // to show class names nicely
+
+            // whitespace
+            { include: '@whitespace' },
+
+            // delimiters and operators
+            [/[{}()\[\]]/, '@brackets'],
+            [/[<>](?!@symbols)/, '@brackets'],
+            [
+              /@symbols/,
+              { cases: { '@operators': 'operator', '@default': '' } },
+            ],
+
+            // @ annotations.
+            // As an example, we emit a debugging log message on these tokens.
+            // Note: message are supressed during the first load -- change some lines to see them.
+            [
+              /@\s*[a-zA-Z_\$][\w\$]*/,
+              { token: 'annotation', log: 'annotation token: $0' },
+            ],
+
+            // numbers
+            [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+            [/0[xX][0-9a-fA-F]+/, 'number.hex'],
+            [/\d+/, 'number'],
+
+            // delimiter: after number because of .\d floats
+            [/[;,.]/, 'delimiter'],
+
+            // strings
+            [/"([^"\\]|\\.)*$/, 'string.invalid'], // non-teminated string
+            [/"/, { token: 'string.quote', bracket: '@open', next: '@string' }],
+
+            // characters
+            [/'[^\\']'/, 'string'],
+            [/(')(@escapes)(')/, ['string', 'string.escape', 'string']],
+            [/'/, 'string.invalid'],
+          ],
+
+          comment: [
+            [/[^\/*]+/, 'comment'],
+            [/\/\*/, 'comment', '@push'], // nested comment
+            ['\\*/', 'comment', '@pop'],
+            [/[\/*]/, 'comment'],
+          ],
+
+          string: [
+            [/[^\\"]+/, 'string'],
+            [/@escapes/, 'string.escape'],
+            [/\\./, 'string.escape.invalid'],
+            [/"/, { token: 'string.quote', bracket: '@close', next: '@pop' }],
+          ],
+
+          whitespace: [
+            [/[ \t\r\n]+/, 'white'],
+            [/\/\*/, 'comment', '@comment'],
+            [/\/\/.*$/, 'comment'],
+          ],
+        },
+      } as MonarchLanguageConfiguration);
+
       setIsMonacoReady(true);
     };
     f();
@@ -60,6 +235,7 @@ const EditorNew = ({
           automaticLayout: true,
         },
       );
+
       setIsEditorCodeMounted(true);
     }
   }, [isMonacoReady, containerRef]);
